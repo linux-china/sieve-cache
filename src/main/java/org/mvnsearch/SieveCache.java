2d814528366ffe7c7712182133828d082bab2dd7
Libing Chen <libing.chen@gmail.com>
2024-01-04 23:14:05 +0800

    refactor: implement Cache interface

--- src/main/java/org/mvnsearch/SieveCache.java
+++ src/main/java/org/mvnsearch/SieveCache.java
@@ -1,18 +1,26 @@
 package org.mvnsearch;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class SieveCache<T> {
+/**
+ * Sieve Cache implementation
+ *
+ * @param <T> object type
+ * @author linux_china
+ */
+public class SieveCache<T> implements Cache<T> {
     private int capacity = 1000;
     private Node<T> head = null;
     private Node<T> tail = null;
     private Node<T> hand = null;
     private int size = 0;
     private final Map<String, Node<T>> store = new ConcurrentHashMap<>();
-
 
     public SieveCache() {
 
@@ -22,7 +30,7 @@ public class SieveCache<T> {
         this.capacity = capacity;
     }
 
-    private void addToHead(Node<T> node) {
+    private void addToHead(@NotNull Node<T> node) {
         node.setNext(this.head);
         node.setPrev(null);
         if (this.head != null) {
@@ -34,7 +42,7 @@ public class SieveCache<T> {
         }
     }
 
-    private void removeNode(Node<T> node) {
+    private void removeNode(@NotNull Node<T> node) {
         if (node.getPrev() != null) {
             node.getPrev().setNext(node.getNext());
         } else {
@@ -62,7 +70,7 @@ public class SieveCache<T> {
     }
 
     @Nullable
-    public T get(String key) {
+    public T get(@NotNull String key) {
         Node<T> node = this.store.get(key);
         if (node != null) {
             node.setVisited(true);
@@ -71,7 +79,7 @@ public class SieveCache<T> {
         return null;
     }
 
-    public void put(String key, T value) {
+    public void put(@NotNull String key, @NotNull T value) {
         synchronized (this) {
             if (this.size >= this.capacity) {
                 this.evict();
@@ -84,12 +92,14 @@ public class SieveCache<T> {
         }
     }
 
-    public void delete(String key) {
-        Node<T> node = this.store.get(key);
-        if (node != null) {
-            this.store.remove(key);
-            this.removeNode(node);
-            this.size = this.size - 1;
+    public void delete(@NotNull String key) {
+        if (this.store.containsKey(key)) {
+            synchronized (this) {
+                Node<T> node = this.store.get(key);
+                this.store.remove(key);
+                this.removeNode(node);
+                this.size = this.size - 1;
+            }
         }
     }
 
@@ -103,6 +113,48 @@ public class SieveCache<T> {
         }
     }
 
+    public boolean containsKey(@NotNull String key) {
+        return this.store.containsKey(key);
+    }
+
+    @Nullable
+    public T getAndPut(@NotNull String key, @NotNull T value) {
+        Node<T> node = this.store.get(key);
+        this.put(key, value);
+        if (node != null) {
+            return node.getValue();
+        }
+        return null;
+    }
+
+    @Nullable
+    public T getAndRemove(@NotNull String key) {
+        Node<T> node = this.store.get(key);
+        if (node != null) {
+            delete(key);
+            return node.getValue();
+        }
+        return null;
+    }
+
+    public boolean putIfAbsent(@NotNull String key, @NotNull T value) {
+        if (!store.containsKey(key)) {
+            put(key, value);
+            return true;
+        }
+        return false;
+    }
+
+    public Map<String, T> getAll(Set<String> keys) {
+        Map<String, T> items = new HashMap<>();
+        for (String key : keys) {
+            if (store.containsKey(key)) {
+                items.put(key, store.get(key).getValue());
+            }
+        }
+        return items;
+    }
+
     public void showItems() {
         Node<T> current = this.head;
         while (current != null) {
@@ -110,5 +162,15 @@ public class SieveCache<T> {
             System.out.println(current.getValue() + "(visited:" + current.isVisited() + ")" + (next != null ? (" -> " + next.getValue()) : "\n"));
             current = next;
         }
+    }
+
+    @Override
+    public int capacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public int size() {
+        return this.store.size();
     }
 }
