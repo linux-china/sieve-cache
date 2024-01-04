f75da7b8f067872664bf55cd4925a8a3adc54ce2
linux_china <libing.chen@gmail.com>
2024-01-04 16:47:57 +0800

    app: Node

---
+++ src/main/java/org/mvnsearch/Node.java
@@ -0,0 +1,54 @@
+package org.mvnsearch;
+
+public class Node {
+    private String key;
+    private Object value;
+    private boolean visited = false;
+    private Node prev = null;
+    private Node next = null;
+
+    public Node(String key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
